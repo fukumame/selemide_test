d8dd3551f9c0b990a1be3f4bafab78c458f037a6
fukumame <tanuma0115@gmail.com>
2015-12-29 20:56:14 +0900

    Changed class name.

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+/**
+ * Created by atsushi-fukuda on 2015/12/28.
+ */
+import org.junit.Test;
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class MainTest {
+
+    @Test
+    public void userCanSearch() {
+        GooglePage page = open("http://google.com/ncr", GooglePage.class);
+        SearchResultsPage results = page.searchFor("selenide");
+        results.getResults().shouldHave(size(10));
+        results.getResult(0).shouldHave(text("Selenide: concise UI tests in Java"));
+    }
+
+}
